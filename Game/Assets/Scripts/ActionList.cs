15f74b9d3f480affb60cc15108c7c732822ea79b
Marcin <Marcingalazka79@gmail.com>
2020-12-02 13:56:13 +0100

    Fix harvest and Action list :D

--- Game/Assets/Scripts/ActionList.cs
+++ Game/Assets/Scripts/ActionList.cs
@@ -5,18 +5,18 @@ using UnityEngine.AI;
 
 public class ActionList : MonoBehaviour
 {
-    public void Move(NavMeshAgent agent, RaycastHit hit, TaskList task )
+    public void Move(NavMeshAgent agent, RaycastHit hit)
     {
         agent.destination = hit.point;
         Debug.Log("Lece!");
-        task = TaskList.Moving;
+   //     task = TaskList.Moving;
     }
 
-    public void Harvest(NavMeshAgent agent, RaycastHit hit, TaskList task, GameObject targetNode)
-    {
-        agent.destination = hit.collider.gameObject.transform.position;
-        Debug.Log("Wydobywam");
-        task = TaskList.Gathering;
-        targetNode = hit.collider.gameObject;
-    }
+   // public void Harvest(NavMeshAgent agent, RaycastHit hit, TaskList task, GameObject targetNode)
+   // {
+   //    agent.destination = hit.collider.gameObject.transform.position;
+   //    Debug.Log("Wydobywam");
+   //    task = TaskList.Gathering;
+   //    targetNode = hit.collider.gameObject;
+   // }
 }
